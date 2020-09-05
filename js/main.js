63b5cdc154a771bde227c13cd3a089abc1fd0dcb
Oleksii Nelin <xsorter@gmail.com>
2020-09-05 23:58:25 +0300

    add filter functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 (function () {
   "use strict";
-  const proructsNode = document.querySelector(".products-box");
-  const cartValue = document.querySelector(".js-value-total");
-  const cartQuantiy = document.querySelector(".js-qty-total");
-  const prices = [];
+  let totalPrice,
+    totalQuantity = 0;
 
-  const handleCart = () => {
-    console.log(prices);
+  const updateCart = () => {
+    document.querySelector(".js-price-total").innerHTML = totalPrice;
+    document.querySelector(".js-qty-total").innerHTML = totalQuantity;
   };
 
-  //handle separate product box
-  const addToCart = (productBox) => {
-    const price = productBox
-      .querySelector("[data-price]")
-      .getAttribute("data-price");
+  const clearCar = () => {};
 
+  //handle single product box
+  const addToCart = (productBox) => {
+    const price = productBox.dataset.price;
     const quantity = productBox.querySelector(".js-qty");
 
     //validation for only positive values
@@ -22,18 +20,65 @@
       quantity.value = 1;
     }
 
-    prices.push({
-      quantity: +quantity.value,
-      price: +price,
-    });
+    totalPrice += quantity.value * price;
+    totalQuantity += +quantity.value;
+    quantity.value = "";
 
-    handleCart();
+    updateCart();
   };
 
-  //get button click with events delegation
-  proructsNode.onclick = () => {
+  const filterProducts = (categoryFilter = 0, priceFilter = 0) => {
+    document.querySelectorAll(".js-product").forEach((el) => {
+      const category = +el.dataset.category;
+      const price = +el.dataset.price;
+      let isCategorySimilar,
+        isPriceSimilar = true;
+
+      //check for price and category correspondencing to filters
+      if (categoryFilter === 0) {
+        isCategorySimilar = true;
+      } else {
+        isCategorySimilar = category === categoryFilter;
+      }
+
+      if (priceFilter === 0) {
+        isPriceSimilar = true;
+      } else {
+        isPriceSimilar = price <= priceFilter;
+      }
+
+      if (isCategorySimilar && isPriceSimilar) {
+        show(el);
+      } else {
+        hide(el);
+      }
+    });
+  };
+
+  //filters change events delegation
+  document.querySelector(".js-filters").onchange = () => {
+    const target = event.target;
+    const categoryFilter = document.querySelector(".js-filter-cat");
+    const priceFilter = document.querySelector(".js-filter-price");
+
+    if (target.tagName != "SELECT") return;
+
+    filterProducts(+categoryFilter.value, +priceFilter.value);
+  };
+
+  //add to cart click events delegation
+  document.querySelector(".js-products").onclick = () => {
     const target = event.target;
     if (!target.classList.contains("js-add")) return;
     addToCart(target.closest(".js-product"));
   };
+
+  //helpers
+  function show(el) {
+    el.hidden = false;
+  }
+
+  function hide(el) {
+    el.hidden = true;
+  }
 })();
